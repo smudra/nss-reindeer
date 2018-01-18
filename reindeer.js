eb5c50d46236b6d473676a3c6a55f732b6d3fb08
Sandhya Rao <smudra@yahoo.com>
2018-01-18 10:56:57 -0600

    added index and reindeer.js files

---
+++ reindeer.js
@@ -0,0 +1,8 @@
+var colors = ["Blue", "Red", "Orange", "Purple", "Hazel", "Aquamarine", "Periwinkle", "Azure", "Fuchsia", "Chocolate", "Amber", "Amaranth"];
+var reindeer = ["Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"];
+
+var hohohoElement = document.getElementById("reindeer");
+
+for (var i = 0; i < reindeer.length; i++) {
+    console.log(colors[i] + " " + reindeer[i]);
+}
